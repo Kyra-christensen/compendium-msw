a71f34940eb19718dfd02def47231986f248135e
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-04 18:30:54 -0700

    created two async fetch functions in futuramaApi.js file

---
+++ src/views/Characters/List.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../../components/Character/Card';
+import Search from '../../components/Character/Search';
+
+export default function List() {
+  return (
+    <div>List</div>
+  )
+}
